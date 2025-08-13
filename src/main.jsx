b3119ee4fea1517004656934a77a28e397779e73
ben85227 <ben85227@gmail.com>
2025-08-13 11:12:54 +0800

    0813-1

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
 // import App from './App.jsx'
-import App from './pages/App0811-weather.jsx'
+import './scss/all.scss'
+import App from './pages/App0813-weather2.jsx'
 
 createRoot(document.getElementById('root')).render(
   //嚴警模式
