646b2d932f5c732ac9eb6fc65e96aa14602377c9
ben85227 <ben85227@gmail.com>
2025-08-20 16:42:35 +0800

    0820

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 // import './index.css'
 // import App from './App.jsx'
 import './scss/all.scss'
-import App from './pages/App0819c.jsx'
+import App from './pages/App0820.jsx'
 import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
